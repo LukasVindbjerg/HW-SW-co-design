09d913171b260c1df745277344c200df5199ef78
Biorrith <60431319+Biorrith@users.noreply.github.com>
2020-12-14 10:44:09 +0100

    Add files via upload

---
+++ Threads Implementation/main.cpp
@@ -0,0 +1,17 @@
+#include "alarm_system.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    AlarmSystem AS;
+
+    AS.activate();
+
+    cout << "Program finished." << endl;
+    return 0;
+}
+
+
+
+
